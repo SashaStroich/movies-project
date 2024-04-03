b29040c6370e606aa9b7a28b2c4b0cf148370493
SashaStroich <sasastroich@gmail.com>
2024-04-03 10:09:46 +0000

    first commit

---
+++ app/models/comment.rb
@@ -0,0 +1,7 @@
+class Comment < ApplicationRecord
+    belongs_to :commentable, polymorphic: true
+    belongs_to :user 
+
+    validates :body, presence: true, length: {minimum: 5, maximum: 500}
+
+end
---
+++ app/services/omdb_client.rb
@@ -0,0 +1,24 @@
+class OmdbClient
+    URL ='http://www.omdbapi.com'
+    
+    def initialize
+      @api_key = Rails.application.credentials.omdb[:apikey]
+    end
+    
+    def search(string)
+      parse_body(
+        Faraday.get(URL, {apikey: @api_key, s: string})
+      )
+    end
+
+    def find_by_title(title)
+      parse_body(
+        Faraday.get(URL, { apikey: @api_key, t: title})
+      )
+    end
+
+    private
+    def parse_body(responce)
+      JSON.parse(responce.body)
+    end
+  end
